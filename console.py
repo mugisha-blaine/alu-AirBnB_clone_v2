73a198a38755abafc4cade5092d5a709efded83b
mugisha-blaine <n.blaine@alustudent.com>
2023-09-29 14:35:57 +0200

    improved

--- console.py
+++ console.py
@@ -5,11 +5,8 @@ import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
-from models.place import Place
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 import models
 
 
