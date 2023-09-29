1f068f16092176246f8669cc37d296326b02ece5
mugisha-blaine <n.blaine@alustudent.com>
2023-09-29 16:31:27 +0200

    improved

--- models/city.py
+++ models/city.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
-from models.place import Place
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
+
     name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            """Getter attribute that returns the list of City instances with
+            state_id equals to the current State.id"""
+            from models import storage
+            city_list = []
+            cities = storage.all("City")
+            for city in cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
