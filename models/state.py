ba3260f3379e21d39d92c5f3943f24ac01449c7c
mugisha-blaine <n.blaine@alustudent.com>
2023-09-29 13:30:16 +0200

    improve

--- models/state.py
+++ models/state.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
