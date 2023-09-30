6d5ba6bb70afc2e421751776b3f2b89a9ae7a81b
mugisha-blaine <n.blaine@alustudent.com>
2023-09-30 23:37:09 +0200

    ok

--- models/base_model.py
+++ models/base_model.py
@@ -40,9 +40,9 @@ class BaseModel:
                 self.created_at = datetime.now()
             if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
