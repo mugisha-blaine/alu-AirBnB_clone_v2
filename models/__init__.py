afb97dee6ae6564b89abfb845b1c2204e6b8a689
mugisha-blaine <n.blaine@alustudent.com>
2023-09-30 22:00:48 +0200

    ok

--- models/__init__.py
+++ models/__init__.py
@@ -6,8 +6,9 @@ if os.getenv("HBNB_TYPE_STORAGE") == "db":
     from models.engine.db_storage import DBStorage
 
     storage = DBStorage()
+    storage.reload()
 else:
 
     from models.engine.file_storage import FileStorage
-
-storage.reload()
+    storage = FileStorage()
+    storage.reload()
