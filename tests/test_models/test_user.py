a76ce7d2cd22d6dc420ab44f42c3b654cb5736a9
mugisha-blaine <n.blaine@alustudent.com>
2023-10-01 05:39:18 -0700

    ok

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.user import User
 
 
